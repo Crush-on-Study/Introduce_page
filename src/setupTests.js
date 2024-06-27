635386cd6045f4f5f3f605fa3f5c76740f9ff6f7
Crush_on_Study <99119068+Crush-on-Study@users.noreply.github.com>
2024-06-27 16:00:01 +0900

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,18 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+class ResizeObserver {
+    constructor(callback) {
+      this.callback = callback;
+    }
+  
+    observe(target) {
+      this.callback([{ target }]);
+    }
+  
+    unobserve(target) {}
+  
+    disconnect() {}
+  }
+  
+  global.ResizeObserver = ResizeObserver;
